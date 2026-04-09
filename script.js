f106b9daf20b4db1acf02ac0ad37c94ec4a8d0a9
Klimaszewska <klimaszewska.justyna@gmail.com>
2026-04-09 10:51:08 +0200

    feature: introduce random delays, improve game state management

--- script.js
+++ script.js
@@ -14,31 +14,98 @@ const minTime = document.getElementById("min-time");
 const maxTime = document.getElementById("max-time");
 const avgTime = document.getElementById("avg-time");
 
-gameArea.addEventListener("click", function() {
-    if (isGameRunning && isWaitingForUserClick) {
-        console.log("Game area clicked");
-    }
-})
+function getRandomDelay() {
+    return Math.floor(Math.random() * 3000) + 1000;
+}
 
-startButton.addEventListener("click", function() {
-    console.log("Start clicked");
-    isGameRunning = true;
+function startNextTry() {
+    if (!isGameRunning) {
+        return;
+    }
+    const randomDelay = getRandomDelay();
+    timeoutId = setTimeout(function () {
+        if (!isGameRunning) {
+            return;
+        }
+        gameArea.style.backgroundColor = "lightgreen";
+        colorChangedAt = Date.now();
+        isWaitingForUserClick = true;
+        console.log("Color changed, waiting for user click");
+    }, randomDelay)
+}
+
+function resetGameState() {
     isWaitingForUserClick = false;
     numberOfTries = 0;
     reactionTimes = [];
+    colorChangedAt = null;
+}
+
+function resetGameArea() {
+    gameArea.style.backgroundColor = "lightgray";
+}
+
+function resetStats() {
     minTime.textContent = "--";
     maxTime.textContent = "--";
     avgTime.textContent = "--";
-    scoreArea.style.display = "block";
-    stopButton.disabled = false;
+    scoreArea.style.display = "none";
+}
+
+function setButtonsOnStart() {
     startButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+function setButtonsOnStop() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+}
+
+function startGame() {
+    console.log("Game started!");
+    resetGameState();
+    resetStats();
+    setButtonsOnStart()
+    resetGameArea();
+    isGameRunning = true;
+    startNextTry();
+}
+
+function endGame() {
+    isGameRunning = false;
+    isWaitingForUserClick = false;
+    setButtonsOnStop();
+    console.log("Game stopped after maximum numbers of tries reached");
+}
+
+gameArea.addEventListener("click", function () {
+    if (isGameRunning && isWaitingForUserClick) {
+        console.log("Game area clicked");
+        let reactionTime = Date.now() - colorChangedAt;
+        reactionTimes.push(reactionTime);
+        numberOfTries++;
+        isWaitingForUserClick = false;
+        scoreArea.style.display = "block";
+        resetGameArea();
+        if (numberOfTries < 5) {
+            startNextTry();
+        } else {
+            endGame();
+        }
+    }
 })
 
-stopButton.addEventListener("click", function() {
+startButton.addEventListener("click", function () {
+    startGame();
+})
+
+stopButton.addEventListener("click", function () {
     console.log("Stop clicked");
     isGameRunning = false;
-    scoreArea.style.display = "none";
     isWaitingForUserClick = false;
-    stopButton.disabled = true;
-    startButton.disabled = false;
+    colorChangedAt = null;
+    setButtonsOnStop();
+    clearTimeout(timeoutId);
+    resetGameArea();
 })
